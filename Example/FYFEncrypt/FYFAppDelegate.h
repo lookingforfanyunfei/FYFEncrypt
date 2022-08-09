5308e0b416cc6ddf4f155833e0440614e346ad6b
范云飞 <fyf786452470@gmail.com>
2022-08-09 19:56:15 +0800

    Initial commit

---
+++ Example/FYFEncrypt/FYFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FYFAppDelegate.h
+//  FYFEncrypt
+//
+//  Created by 786452470@qq.com on 08/09/2022.
+//  Copyright (c) 2022 786452470@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FYFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/FYFEncrypt/FYFViewController.h
@@ -0,0 +1,13 @@
+//
+//  FYFViewController.h
+//  FYFEncrypt
+//
+//  Created by 786452470@qq.com on 08/09/2022.
+//  Copyright (c) 2022 786452470@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FYFViewController : UIViewController
+
+@end
